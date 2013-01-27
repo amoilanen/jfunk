8b353df2c6fee7303d69cc861eb3c751c7329af8
Anton Ivanov <anton.al.ivanov@gmail.com>
2013-01-27 20:01:06 +0100

    Added JavaDoc documentation for the implemented Set methods

--- src/main/java/org/common/jfunk/Sets.java
+++ src/main/java/org/common/jfunk/Sets.java
@@ -1,15 +1,21 @@
 package org.common.jfunk;
 
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
 /**
- * Utility functions for working with sets. Result is always a {@link java.util.HashSet}
+ * Utility functions for working with sets similar to those available for Ruby sets. 
+ * Result is always a {@link java.util.HashSet}
  * 
  */
 public class Sets {
     
+    /**
+     * Constructs a new set from the provided elements.
+     * 
+     * @param elems elements to construct the set from
+     * @return set that includes all the elements {@code elems}
+     */
     public static <T> Set<T> set(T... elems) {
         Set<T> result = new HashSet<T>();
 
@@ -19,6 +25,12 @@ public class Sets {
         return result;
     }
     
+    /**
+     * Computing the union of two sets
+     * 
+     * @param sets - sets to be united
+     * @return union of the sets
+     */
     @SuppressWarnings("unchecked")
     public static <T> Set<T> union(Set<?>... sets) {
         Set<T> result = new HashSet<T>();
@@ -29,6 +41,12 @@ public class Sets {
         return result;
     }
     
+    /**
+     * Computing the intersection of two sets
+     * 
+     * @param sets - sets to be intersected
+     * @return intersection of the sets
+     */
     public static <T> Set<T> intersection(Set<?>...sets) {        
         @SuppressWarnings("unchecked")
         Set<T> result = (sets.length > 0) ? (Set<T>) sets[0] : new HashSet<T>();
@@ -43,6 +61,11 @@ public class Sets {
     //symmetric difference
     //isSubset
     //isSuperset
+    
+ 
+    //#classify
+    //#divide
+    //#flatten
     
     @SuppressWarnings("unchecked")
     private static <T> Set<T> intersectionOfTwo(Set<?> x, Set<?> y) {
