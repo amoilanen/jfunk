900212aae83a5f433616884e535371cc17d5904e
Anton Ivanov <anton.al.ivanov@gmail.com>
2013-03-07 23:25:00 +0100

    Added JavaDoc comments to the implemented methods for Iterators

--- src/main/java/org/common/jfunk/Iterators.java
+++ src/main/java/org/common/jfunk/Iterators.java
@@ -13,20 +13,66 @@ import java.util.List;
  */
 public class Iterators {
     
-    public static <T, U> Iterator<U> map(Iterator<T> iterator, Function<T, U> f) {
-        return new MappedIterator<T, U>(iterator, f);
+    /**
+     * Maps the provided function {@code f} over iterator {@code i} in a lazy fashion: the
+     * elements of the iterator {@code i} are not requested until the corresponding element
+     * has been requested on the iterator returned from {@code map}.
+     * 
+     * @param i - iterator to map the function over
+     * @param f - function to map
+     * @return iterator each element of which is a result of an application of the function {@code f}
+     * to the corresponding element of the original iterator {@code i}
+     */
+    public static <T, U> Iterator<U> map(Iterator<T> i, Function<T, U> f) {
+        return new MappedIterator<T, U>(i, f);
     }
     
-    public static <T> Iterator<T> filter(Iterator<T> iterator, Predicate<T> p) {        
-        return new FilteredIterator<T>(iterator, p);
+    /**
+     * Filters the provided iterator {@code i} with the predicate {@code p} in a lazy fashion: the
+     * elements of the iterator {@code i} are not requested until the corresponding element
+     * has been requested on the iterator returned from {@code filter}.
+     * 
+     * @param i - iterator to filter
+     * @param p - predicate which will be used to determine which elements should be filtered out
+     * @return iterator over all the elements of the iterator {@code i} for which the predicate {@code p}
+     * returns {@code true}
+     */
+    public static <T> Iterator<T> filter(Iterator<T> i, Predicate<T> p) {        
+        return new FilteredIterator<T>(i, p);
     }
     
+    /**
+     * Merges the provides iterators {@code iterators}.<br/>
+     * <h3>Example:</h3>
+     * 
+     * First iterator [1, 2, 3, 4, 5] <br/>
+     * Second iterator [6, 7, 8, 9, 10] <br/>
+     * <br/>
+     * then the merged iterator is [1, 6, 2, 7, 3, 8, 4, 9, 5, 10]<br/>
+     * 
+     * @param iterators - iterators to merge
+     * @return iterator over all the elements of the argument iterators which were merged like in the provided example:
+     * first go the first elements of the iterators, then the second elements and so forth
+     */
     public static <T> Iterator<T> merge(Iterator<?>... iterators) {
         final List<Iterator<?>> iters = Arrays.<Iterator<?>>asList(iterators);
         
         return new MergedIterator<T>(iters);
     }
 
+    /**
+     * Concatenates the provided iterators {@code iterators}.<br/>
+     * <h3>Example:</h3>
+     * 
+     * First iterator [1, 2, 3, 4, 5] <br/>
+     * Second iterator [6, 7, 8, 9, 10] <br/>
+     * <br/>
+     * then the concatenated iterator is [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]<br/>
+     * 
+     * @param iterators - iterators to concatenate
+     * @return iterator over all the elements of the argument iterators which were concatenated like in the provided example:
+     * first go the elements of the first iterator, then the elements of the second iterator and so forth
+     */
     public static <T> Iterator<T> concat(Iterator<?>... iterators) {        
         @SuppressWarnings("unchecked")
         Iterator<T> result = iterators.length > 0 ? (Iterator<T>) iterators[0] : Collections.<T>emptyList().iterator();
@@ -37,18 +83,30 @@ public class Iterators {
         return result;
     }
     
-    public static <T> List<T> toList(Iterator<T> iter) {
+    /**
+     * Returns all the elements of the provided iterator {@code i} as a list.
+     * 
+     * @param i - iterator elements of which should be returned in a list
+     * @return list of all the elements in the provided iterator
+     */
+    public static <T> List<T> toList(Iterator<T> i) {
         List<T> result = new ArrayList<T>();
         
-        while (iter.hasNext()) {
-            result.add(iter.next());
+        while (i.hasNext()) {
+            result.add(i.next());
         };
         return result;
     }
 
+    /**
+     * Returns all the elements of the provided iterator {@code i} as an array.
+     * 
+     * @param i - iterator elements of which should be returned in an array
+     * @return array of all the elements in the provided iterator
+     */
     @SuppressWarnings("unchecked")
-    public static <T> T[] toArray(Iterator<T> iter, Class<T> clazz) {
-        return (T[]) toList(iter).toArray((T[]) Array.newInstance(clazz, 0));
+    public static <T> T[] toArray(Iterator<T> i, Class<T> clazz) {
+        return (T[]) toList(i).toArray((T[]) Array.newInstance(clazz, 0));
     }
 
     //TODO: Implement the following remaining methods
